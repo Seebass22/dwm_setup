4da174ec60a01dffbb7263f4782a0a04a4b26fdb
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2019-10-04 08:16:58 +0200

    added spawn firefox bind

--- config.h
+++ config.h
@@ -59,6 +59,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *roficmd[]  = { "rofi", "-modi", "drun", "-show", "drun", NULL };
 static const char *termcmd[]  = { "urxvt", NULL };
+static const char *firefoxcmd[]  = { "firefox", NULL };
 static const char *bookcmd[]  = { "/home/seb22/bashscripts/books.sh", NULL };
 static const char *symcmd[]  = { "/home/seb22/bashscripts/dmenu_symbols.sh", NULL };
 
@@ -66,6 +67,7 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	//{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_p,      spawn,          {.v = roficmd } },
+	{ MODKEY,                       XK_w,      spawn,          {.v = firefoxcmd } },
 	{ MODKEY|ShiftMask,             XK_x,      spawn,          {.v = bookcmd } },
 	{ MODKEY|ShiftMask,             XK_s,      spawn,          {.v = symcmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
